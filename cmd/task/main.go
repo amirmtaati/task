8e040b5e9067483f31ad0e51b8d8cdcd4119a15a
amirmtaati <amirmtaati@gmail.com>
2025-08-01 10:07:58 +0330

    task struct removed from main.go

--- cmd/task/main.go
+++ cmd/task/main.go
@@ -2,18 +2,6 @@ package main
 
 import "fmt"
 
-type Task struct {
-	ID              int
-	raw             string
-	done            bool
-	priority        string
-	completion_date string
-	creation_date   string
-	todo            string
-	project         string
-	context         string
-	tags            map[string]string
-}
 
 func main() {
 	fmt.Println("Hello Tempus!")
